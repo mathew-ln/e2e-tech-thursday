3a8a325a1be7597e72d3453bcb412751595c44ce
Mathew Botha <botham@LEGAL.REGN.NET>
2023-06-15 13:47:04 +0200

    Add the test unsuccessfully again

--- Angular/e2e/cypress/e2e/step-definitions/LandingPage.spec.ts
+++ Angular/e2e/cypress/e2e/step-definitions/LandingPage.spec.ts
@@ -1,5 +1,7 @@
 import { Given, When, Then, And} from "cypress-cucumber-preprocessor/steps";
+import { ElementRef, Renderer2 } from '@angular/core';
 
+//-------------------------------------------------------------------------
 Given(/^the user is on the landing page$/, () => {
 	cy.visit("http://localhost:4200/dashboard");
 });
@@ -7,3 +9,82 @@ Given(/^the user is on the landing page$/, () => {
 Then(/^the user shall see the employee dashboard$/, () => {
 	return true;
 });
+
+//-------------------------------------------------------------------------
+Given(/^the user is on the landingpage$/, () => {
+	cy.visit("http://localhost:4200/dashboard");
+});
+
+Then(/^the user shall see the top 4 heroes$/, () => {
+	return true;
+});
+
+//-------------------------------------------------------------------------
+Given(/^the user is on the landingpage$/, () => {
+	cy.visit("http://localhost:4200/dashboard");
+});
+
+When('the user clicks on a {string}', (HeroName: string) => {
+	cy.visit("http://localhost:4200/detail/15");
+});
+
+Then(/^the user is redirected to <Page>$/, () => {
+	return true;
+});
+
+//-------------------------------------------------------------------------
+Given(/^the user is on the landingpage$/, () => {
+	cy.visit("http://localhost:4200/dashboard");
+});
+
+When(/^the user clicks on the heroes button$/, () => {
+	cy.visit("http://localhost:4200/heroes");
+});
+
+Then(/^the user is redirected to the <Page>$/, () => {
+	return true;
+});
+
+//-------------------------------------------------------------------------
+Given(/^the user is on the heroes page$/, () => {
+	cy.visit("http://localhost:4200/heroes");
+});
+
+When(/^the user clicks on the delete button on a hero$/, () => {
+	cy.click()
+	return true;
+});
+
+Then(/^the hero is deleted$/, () => {
+	return true;
+});
+//-------------------------------------------------------------------------
+
+Given(/^the user is on the heroes page$/, () => {
+	cy.visit("http://localhost:4200/heroes");
+});
+
+When(/^the user types input box <NewHero> and clicks the add hero button$/, () => {
+	return true;
+});
+
+Then(/^a new hero is added to the hero list and the number of heroes increase$/, () => {
+	return true;
+});
+//-------------------------------------------------------------------------
+
+Given(/^the user is on the heroes page$/, () => {
+	cy.visit("http://localhost:4200/heroes");
+});
+
+When(/^the user clicks on the add hero button without inputting a HeroName$/, () => {
+	return true;
+});
+
+Then(/^nothing happens$/, () => {
+	return true;
+});
+
+
+
+
